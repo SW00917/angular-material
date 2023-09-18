8adad7a652e0a7786214b3e8bfe359ddf7ba37da
weihsu <weihsu@capgemini.com>
2023-09-18 17:57:55 +0800

    使用ButtonToggle與ButtonToggleGroup

--- src/app/shared-material/shared-material.module.ts
+++ src/app/shared-material/shared-material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [MatButtonModule, MatButtonToggleModule],
+  exports: [MatButtonModule, MatButtonToggleModule],
 })
 export class SharedMaterialModule {}
